4fa732c050d441d42bd4b2610c9f40e63cbed705
pramod <pramodacharya808@gmail.com>
2023-07-09 15:41:32 +0530

    Ceiling of a number Known sorted array

---
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BinarySearch/CeilingNumber.java
@@ -0,0 +1,39 @@
+/*Ceiling of Number:
+Given a target element, we have to find the smallest element in an array,
+which is greater than or equal to the TARGET element.
+
+Ex: arr = [2,3,4,6,9,15,79]
+    target = 7
+    elements greater than target are 9, 15, 79
+    The smallest element which is greater or equal to the Target element is ==== 9
+*/
+
+package Array.InterviewQuestions.BinarySearch;
+
+public class CeilingNumber {
+    public static void main(String[] args){
+        int[] arr = {1,3,5,8,10,15,17,19,38};
+        int key = 11;
+        int ans = Ceil(arr,key);
+        System.out.println("The Ceiling of "+key+" in the given array is "+ans);
+    }
+
+    public static int Ceil(int[] arr1, int key){
+        int start = 0;
+        int end = arr1.length;
+        int mid;
+        while(start <= end){
+            mid = (start+end)/2;
+
+            if (arr1[mid]>key){
+                end = mid-1;
+            }
+            else if(arr1[mid]<key){
+                start = mid+1;
+            }
+            else
+                return arr1[mid];
+        }
+        return arr1[start];
+    }
+}
