dd947b1e5195dcb02a29ec66d253c02c63a6ce60
pramod <pramodacharya808@gmail.com>
2023-07-13 20:25:53 +0530

    today java

--- Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BinarySearch/Playground.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BinarySearch/Playground.java
@@ -2,7 +2,7 @@ package Array.InterviewQuestions.BinarySearch;
 
 
 //Ceiling of a number using Binary Search
-public class Playground {
+/*public class Playground {
     public static void main(String[] args){
         int[]  arr = {1,3,4,6,8,9};
         int key = 5;
@@ -28,5 +28,32 @@ public class Playground {
                 return arr[mid];
         }
         return res;
+    }
+}*/
+
+
+public class Playground{
+    public static void main(String[] args){
+        int[] arr = {1,2,4,6,8,9,10};
+        int key = 9 ;
+        System.out.println(Ceiling(arr, key));
+    }
+
+    public static int Ceiling (int[] arr, int key){
+        int start = 0;
+        int end = arr.length;
+
+        while (start <= end) {
+            int mid=(start+end)/2;
+            if(arr[mid]>key){
+                end = mid-1;
+            }
+            else if(arr[mid]<key){
+                start = mid+1;
+            }
+            else
+                return arr[mid];
+        }
+        return arr[start];
     }
 }
