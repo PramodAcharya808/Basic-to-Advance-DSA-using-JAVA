71391f18a0fc2abdff7060122d6f9b0073d45759
pramod <pramodacharya808@gmail.com>
2023-07-20 18:45:52 +0530

    Bubble sort

---
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/LinearSearch/SearchLinear.java
@@ -0,0 +1,17 @@
+package Array.InterviewQuestions.LinearSearch;
+
+public class SearchLinear {
+    public static void main(String[] args){
+        int[] a={1,2,3,4,67,34,3,6,89,6,45,7,655};
+        int key = 89;
+        System.out.println(Search(a, key));
+
+    }
+    static int Search(int[] arr, int key){
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == key)
+                return i;
+        }
+        return -1;
+    }
+}
