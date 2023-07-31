9ae43b716151be9ae664369cf8b9b13afca442c5
pramod <pramodacharya808@gmail.com>
2023-07-31 20:57:23 +0530

    added bubble sort 1

--- Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
@@ -2,6 +2,6 @@ package Array.InterviewQuestions.BubbleSort;
 
 public class Bubble_1 {
     public static void main (String[] args){
-        int[] arr = {2, 1, 4, 6, 5, };
+        int[] arr = {2, 1, 4, 6, 5, 3};
     }
 }
