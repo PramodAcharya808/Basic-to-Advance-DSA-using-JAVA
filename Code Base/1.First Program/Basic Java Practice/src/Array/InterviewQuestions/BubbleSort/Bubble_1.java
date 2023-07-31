24c1778387a102521b14af4486aa05bce082e5f9
pramod <pramodacharya808@gmail.com>
2023-07-31 21:00:49 +0530

    added bubble sort 1

--- Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
@@ -7,7 +7,7 @@ public class Bubble_1 {
         int[] arr = {2, 1, 4, 6, 5, 3, 0};
         System.out.println(Arrays.toString(arr));
         System.out.println("After sorting in ascending ");
-        BubbleSort
+        BubbleSort(arr)
         System.out.println(Arrays.toString(arr));
     }
 }
