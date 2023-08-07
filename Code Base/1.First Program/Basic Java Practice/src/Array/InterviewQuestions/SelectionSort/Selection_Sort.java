9132ef3ec43b2c96e9c8dc7b87578b5daac23c3f
pramod <pramodacharya808@gmail.com>
2023-08-07 19:48:18 +0530

    Selection Sort

--- Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/SelectionSort/Selection_Sort.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/SelectionSort/Selection_Sort.java
@@ -8,7 +8,7 @@ public class Selection_Sort {
 
     public static void Sort(int[] arr){
         for (int i=0; i<arr.length ;i++){
-            int max= getmax(arr, )
+            int max= getmax(arr, );
         }
     }
 }
