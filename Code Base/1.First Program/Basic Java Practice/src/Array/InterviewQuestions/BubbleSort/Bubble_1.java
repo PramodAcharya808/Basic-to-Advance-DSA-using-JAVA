9dd5ac6a02ae74f103b0db42b1fef3f7f96863c7
pramod <pramodacharya808@gmail.com>
2023-08-07 18:44:43 +0530

    bubble sort

--- Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
@@ -3,24 +3,7 @@ package Array.InterviewQuestions.BubbleSort;
 import java.util.Arrays;
 
 public class Bubble_1 {
-    public static void main (String[] args){
-        int[] arr = {2, 1, 4, 6, 5, 3, 0};
-        System.out.println(Arrays.toString(arr));
-        System.out.println("After sorting in ascending ");
-        BubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-
-    public static void BubbleSort (int[] arr){
-        int temp;
-        for (int i=0 ; i<arr.length ; i++){
-            for (int j=1 ; j < arr.length-i ; j++){
-                if (arr[j] < arr[j-1]){
-                    temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                }
-            }
-        }
+    public static void main(String[] args) {
+        int 
     }
 }
