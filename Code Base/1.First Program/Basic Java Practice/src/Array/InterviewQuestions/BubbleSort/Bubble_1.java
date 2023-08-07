05b32207a9d22439ca544df57933aab62eef50ab
pramod <pramodacharya808@gmail.com>
2023-08-07 19:04:49 +0530

    bubble sort

--- Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/BubbleSort/Bubble_1.java
@@ -26,10 +26,10 @@ public class Bubble_1 {
         int temp;
         for (int i=0 ; i<arr.length; i++){
             for (int j=1 ; j<arr.length-i; j++){
-                if (arr[j] > arr[j-1]){
-                    temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                if (arr[j] < arr[j-1]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
