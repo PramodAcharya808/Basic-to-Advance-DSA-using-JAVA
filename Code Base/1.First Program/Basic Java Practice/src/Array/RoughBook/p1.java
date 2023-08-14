4e7733916b11f96f3ad95bb60973293d9878da53
pramod <pramodacharya808@gmail.com>
2023-08-14 19:43:14 +0530

    p1 bubble bubble sort practice

--- Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/p1.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/p1.java
@@ -1,8 +1,23 @@
 package Array.RoughBook;
 
+import java.util.Arrays;
+
 public class p1 {
     public static void main(String[] args){
         int[] arr = {34,5,12,4,7,9,2};
-        
+        Bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void Bubble(int[] arr){
+        int temp;
+        for(int i=0; i<arr.length;i++){
+            for (int j=1; j<arr.length-i; j++){
+                if(arr[j] < arr[j-1]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
