c79edf7419a0dc675734b30fd908c661a4cee358
pramod <pramodacharya808@gmail.com>
2023-08-14 21:10:36 +0530

    insertion sort added

--- Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/SortingAlgo/InsertionSort/InsertionSort.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/InterviewQuestions/SortingAlgo/InsertionSort/InsertionSort.java
@@ -6,9 +6,8 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {12,34,56,2,4,1,0,8,3,-9,-5,-1};
         Sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println (Arrays.toString(arr));
     }
-
     public static void Sort(int[] arr){
         for (int  i=0 ; i<=arr.length-2 ; i++){
             for (int j=i+1 ; j>0 ; j--){
