c35b20ec82f509262adb18fa7e946f4685b5d29b
pramod <pramodacharya808@gmail.com>
2023-08-14 19:53:37 +0530

    p1 bubble bubble sort practice

--- Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/p2.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/p2.java
@@ -1,4 +1,30 @@
 package Array.RoughBook;
 
+import java.util.Arrays;
+
 public class p2 {
+    public static void main(String[] args) {
+        int[] arr={23,5,78,1,9,24,7,3,45};
+        Selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void Selection(int[] arr){
+        for (int i=0 ; i<arr.length; i++){
+            int lastindex = arr.length-1-i;
+            int maxnumindex = GetMax(arr, 0, lastindex);
+            int temp = arr[maxnumindex];
+            arr[maxnumindex] = arr[lastindex];
+            arr[lastindex] = temp;
+        }
+    }
+
+    public static int GetMax(int[] arr, int start, int end){
+        int maxindex=0;
+        for (int i = start; i<=end; i++){
+            if(arr[i] > maxindex)
+                maxindex = i;
+        }
+        return maxindex;
+    }
 }
