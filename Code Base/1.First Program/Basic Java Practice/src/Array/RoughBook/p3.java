6cdff03b6c3bdb9e578948c9171fde62221ca4e7
pramod <pramodacharya808@gmail.com>
2023-08-14 20:31:49 +0530

    p3 bubble selection sort practice

--- Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/p3.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/p3.java
@@ -10,9 +10,9 @@ public class p3 {
     }
 
     public static void Bubble(String[] str){
-        for(int i=0; i<str.length; i++){
-            for(int j=1; j<=str.length-i; j++){
-                if (str[j].compareTo(str[j-1])>0){
+        for(int i=0; i<=str.length-1; i++){
+            for(int j=1; j<=str.length-i-1; j++){
+                if (str[j].compareTo(str[j-1])<0){
                     String temp = str[j-1];
                     str[j] = str[j-1];
                     str[j-1]=temp;
