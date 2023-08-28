00e710659dca6c311d42b4ee9625e9faf2b4c1e6
pramod <pramodacharya808@gmail.com>
2023-08-28 19:30:14 +0530

    test javadsa commit

---
+++ Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/Rough1.java
@@ -0,0 +1,8 @@
+package Array.RoughBook;
+
+public class Rough1 {
+    public static void main(String[] args) {
+        int[] bin1 = {2,4,7,9,12,34,56,78,90};
+        System.out.println("");
+    }
+}
