739ec2c2875632db68cb2cecde1dfacd8ec6a693
pramod <pramodacharya808@gmail.com>
2023-08-28 20:03:40 +0530

    test javadsa commit

--- Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/Rough1.java
+++ Code Base/1.First Program/Basic Java Practice/src/Array/RoughBook/Rough1.java
@@ -4,12 +4,12 @@ public class Rough1 {
     public static void main(String[] args) {
         int[] bin1 = {2,4,7,9,12,34,56,78,90};
         int key=56;
-        System.out.println(key+" is at "+BinarySearch(bin1, key));
+        String name = "Pramod";
+        System.out.println(key+" is at "+BinarySearch(bin1, key,));
     }
     public static int BinarySearch (int[] arr1, int key){
         int start=0;
         int end=arr1.length-1;
-
         while(start<=end){
             int mid = (start+end)/2;
             if(arr1[mid] > key)
@@ -21,4 +21,5 @@ public class Rough1 {
         }
         return -1;
     }
+
 }
